7f6a526a325f6bc34d910dbd6373ec02af858c21
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-09-25 21:42:44 +0800

    Added the api key and url

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ import axios from 'axios';
 const app = express();
 const port = 3000;
 
+const API_URL = "https://api.humorapi.com/jokes/random";
+const apiKey = "d2b9afca979e432187a5c7ac238a31f9";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 });
+
+app.get('/jokes', async(req, res) => {
+    try {
+        const result = await axios.get()
+
+    } catch(error) {
+        res.status(404).send({ message: error.message });
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
